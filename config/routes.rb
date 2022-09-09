2f6ec1ca90f2045b187001b767d6a1fa64bc1ebf
jlmansilla <juanmancillaster@gmail.com>
2022-09-08 22:21:10 -0400

    Bootstrap change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :newspapers
   resources :categories
+  root 'newspapers#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
