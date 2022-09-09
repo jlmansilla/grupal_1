c36cbdad0c45276eab6ea8c9570df933aabcf3ba
jlmansilla <juanmancillaster@gmail.com>
2022-09-08 21:51:54 -0400

    Seed newspaper

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-"create category"
+puts "create category"
 Category.create(name: 'Deportes')
 Category.create(name: 'Farándula')
 Category.create(name: 'Nacional')
 Category.create(name: 'Internacional')
 Category.create(name: 'Política')
 
+puts "create newspaper"
+Newspaper.create(header: 'Flamengo campeón', body: 'Flamengo campeón de la copa libertadores', link:"https://www.conmebol.com/noticias/por-la-gloria-eterna-paranaense-y-flamengo-jugaran-la-final-de-conmebol-libertadores/" category: Category.id(1))
+Newspaper.create(header: 'Flamengo campeón', body: 'Flamengo campeón de la copa libertadores', link:"https://www.conmebol.com/noticias/por-la-gloria-eterna-paranaense-y-flamengo-jugaran-la-final-de-conmebol-libertadores/" category: Category.id(3))
+Newspaper.create(header: 'Flamengo campeón', body: 'Flamengo campeón de la copa libertadores', link:"https://www.conmebol.com/noticias/por-la-gloria-eterna-paranaense-y-flamengo-jugaran-la-final-de-conmebol-libertadores/" category: Category.id(4))
