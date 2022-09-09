5cc17e470b9010cc0f1e3300af0e384d7049e074
jlmansilla <juanmancillaster@gmail.com>
2022-09-08 21:42:51 -0400

    Seed category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+"create category"
+Category.create(name: 'Deportes')
+Category.create(name: 'Farándula')
+Category.create(name: 'Nacional')
+Category.create(name: 'Internacional')
+Category.create(name: 'Política')
+
