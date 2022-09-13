a9e6d397cc9313bfa39405b50312234f682517f9
jlmansilla <juanmancillaster@gmail.com>
2022-09-13 19:49:31 -0300

    link to home

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,7 +1,7 @@
 class FiltersController < ApplicationController
   def index
     @newspapers = Newspaper.where(category_id: params[:q] )
-    #debuger
+    
     #Newspaper.find(params[:id])
     
   end
